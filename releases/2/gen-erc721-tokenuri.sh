ccad56cf5969f31e225330f7abc7992ec092e9b6
duststars <i@exploro.one>
2025-08-22 03:27:04 +0800

    improve qsl card, size is reduced

---
+++ releases/2/gen-erc721-tokenuri.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+scriptPath=$(realpath $0)
+scriptDir=$(dirname $scriptPath)
+
+cd "$scriptDir"
+
+IMAGE=$(base64 -i card.svg)
+IMAGE="data:image/svg+xml;base64,${IMAGE}"
+
+METADATAJSON=$(yq -o json metadata.yaml | jq --compact-output --arg urlarg "$IMAGE" '.image = $urlarg' | base64)
+
+ERC721TOKENURI="data:application/json;base64,${METADATAJSON}"
+
+echo $ERC721TOKENURI
